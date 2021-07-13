d627132b8a1425712514fd10c84617ece36c269f
Frank Wei <frank@frankwei.xyz>
2021-07-13 12:42:34 +0800

    good to gen

---
+++ src/entities/User.entity.ts
@@ -0,0 +1,22 @@
+import { PrimaryGeneratedColumn, Column, Entity } from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  address: string;
+
+  @Column()
+  username: string;
+
+  @Column({ default: 'https://i.loli.net/2021/05/13/CiEFPgWJzuk5prZ.png' })
+  avatar: string;
+
+  @Column()
+  nickname: string;
+
+  @Column()
+  description: string;
+}
