5c69e681a21ee992f44aa93f4f4253f1ee0e1aa5
Casimir Crystal <casimir.crystal.blue@outlook.com>
2021-07-13 12:35:02 +0800

    update: fix typo and add entity to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,21 +1,24 @@
 import { Module } from '@nestjs/common';
 import { JWT_KEY } from './constants';
 import { JwtModule } from '@nestjs/jwt';
+import { User } from './users/entities/User.entity';
 import { AuthModule } from './auth/auth.module';
 import { LoginModule } from './login/login.module';
 import { UsersModule } from './users/users.module';
 import { ConfigModule } from '@nestjs/config';
 import { SystemModule } from './system/system.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppService } from './app.service';
+import { AppController } from './app.controller';
+import * as fs from 'fs';
+
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 require('dotenv').config({
   path:
     process.env.NODE_ENV === 'production'
       ? '.env.production'
-      : '.env.devlopment',
+      : '.env.development',
 });
-import { AppService } from './app.service';
-import { AppController } from './app.controller';
 
 @Module({
   imports: [
@@ -40,7 +43,7 @@ import { AppController } from './app.controller';
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
       autoLoadEntities: true,
-      entities: [],
+      entities: [User],
       // shouldn't be used in production - otherwise you can *lose* production data.
       synchronize: true,
     }),
