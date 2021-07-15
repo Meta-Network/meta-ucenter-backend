c0bc45fdc5192b858ff8d5aa4c1d8139845be200
Frank Wei <frank@frankwei.xyz>
2021-07-15 20:16:27 +0800

    feat: good to use email code

--- src/two-factor-auth/two-factor-auth.service.ts
+++ src/two-factor-auth/two-factor-auth.service.ts
@@ -11,6 +11,8 @@ import { TotpStrategy } from './totp.strategy';
 import { Repository } from 'typeorm';
 import { TwoFactorAuth } from 'src/entities/TwoFactorAuth.entity';
 import { InjectRepository } from '@nestjs/typeorm';
+import { VerificationCodeService } from 'src/verification-code/verification-code.service';
+import { EmailStrategy } from './email.strategy';
 
 @Injectable()
 export class TwoFactorAuthService {
@@ -19,6 +21,8 @@ export class TwoFactorAuthService {
     private readonly tfaRepo: Repository<TwoFactorAuth>,
     @InjectRepository(User)
     private readonly userRepo: Repository<User>,
+    private readonly verificationCodeService: VerificationCodeService,
+    private readonly emailStrategy: EmailStrategy,
   ) {}
 
   async get2FADetailFor(userId: number, type: TwoFactorType) {
@@ -34,6 +38,36 @@ export class TwoFactorAuthService {
     return Boolean(target);
   }
 
+  async requestChallenge(userId: number, type: TwoFactorType) {
+    const isTypeExist = await this.isTypeExistFor(userId, type);
+
+    const user = await this.userRepo.findOne(userId);
+
+    const NoNeedTypes = [TwoFactorType.TOTP];
+
+    if (NoNeedTypes.includes(type)) {
+      throw new BadRequestException(
+        `No Challenge for Type ${type}, just verify.`,
+      );
+    }
+
+    if (type !== TwoFactorType.EmailCode && !isTypeExist)
+      throw new BadRequestException('Type not exist');
+
+    switch (type) {
+      case TwoFactorType.EmailCode: {
+        await this.emailStrategy.generateVerificationCodeForEmail(
+          user.username,
+        );
+        break;
+      }
+      default:
+        throw new NotImplementedException(
+          `2FA Challenge for type ${type} was not implemented`,
+        );
+    }
+  }
+
   async bind2FA(type: TwoFactorType, _user: Partial<User>) {
     const isTypeExist = await this.isTypeExistFor(_user.id, type);
 
@@ -43,6 +77,10 @@ export class TwoFactorAuthService {
     const user = await this.userRepo.findOne(_user.id);
 
     switch (type) {
+      // Email
+      case TwoFactorType.EmailCode: {
+        throw new BadRequestException('Please bind your email in the settings');
+      }
       case TwoFactorType.TOTP: {
         const { secret, otpauth } = await TotpStrategy.generate(user.username);
         // @todo: save the 2FA secret into the DB, for future verification
@@ -82,6 +120,17 @@ export class TwoFactorAuthService {
     userId: number,
     code: string,
   ): Promise<boolean> {
+    if (type === TwoFactorType.EmailCode) {
+      const user = await this.userRepo.findOne(userId);
+      return this._verify(
+        {
+          type: TwoFactorType.EmailCode,
+          user,
+        },
+        code,
+      );
+    }
+
     const detail = await this.get2FADetailFor(userId, type);
     if (!detail) throw new NotFoundException(`No found for type: '${type}'`);
     if (!detail.isEnabled)
@@ -89,8 +138,15 @@ export class TwoFactorAuthService {
     return this._verify(detail, code);
   }
 
-  async _verify(detail: TwoFactorAuth, code: string): Promise<boolean> {
+  async _verify(
+    detail: Partial<TwoFactorAuth>,
+    code: string,
+  ): Promise<boolean> {
     switch (detail.type) {
+      case TwoFactorType.EmailCode: {
+        const email = detail.user.username;
+        return this.emailStrategy.verify(email, code);
+      }
       case TwoFactorType.TOTP: {
         const { secret } = detail;
         return TotpStrategy.validate(code, secret);
