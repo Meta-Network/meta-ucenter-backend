4048a0d7f1ebc97a8ebff9e0733f87727ca4d440
Frank Wei <frank@frankwei.xyz>
2021-07-16 15:49:07 +0800

    feat: new computed field `is2FAEnabled`

--- src/entities/User.entity.ts
+++ src/entities/User.entity.ts
@@ -5,6 +5,10 @@ import {
   UpdateDateColumn,
   PrimaryGeneratedColumn,
   OneToMany,
+  AfterLoad,
+  JoinTable,
+  AfterInsert,
+  AfterUpdate,
 } from 'typeorm';
 import { TwoFactorAuth } from './TwoFactorAuth.entity';
 
@@ -31,6 +35,20 @@ export class User {
   @UpdateDateColumn()
   updated_at: Date;
 
-  @OneToMany(() => TwoFactorAuth, (twa) => twa.user)
+  @OneToMany(() => TwoFactorAuth, (twa) => twa.user, { eager: true })
+  @JoinTable()
   twoFactors: TwoFactorAuth[];
+
+  // it should be computed by the fn below:
+  // by defeault it is false.
+  is2FAEnabled = false;
+
+  // update in object when load inserted or update
+  // no DB writing for this.
+  @AfterLoad()
+  @AfterInsert()
+  @AfterUpdate()
+  keepingTrackOf2FAStatus(): void {
+    if (this.twoFactors) this.is2FAEnabled = this.twoFactors.length > 0;
+  }
 }
