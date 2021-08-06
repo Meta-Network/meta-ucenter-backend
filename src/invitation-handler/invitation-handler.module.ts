eff60efd0754325b7c41dcd9247c3d04eeade619
Casimir Crystal <casimir.crystal.blue@outlook.com>
2021-08-06 16:39:42 +0800

    fix: invitation handler doesn't have controller

--- src/invitation-handler/invitation-handler.module.ts
+++ src/invitation-handler/invitation-handler.module.ts
@@ -1,9 +1,7 @@
 import { Module } from '@nestjs/common';
 import { InvitationHandlerService } from './invitation-handler.service';
-import { InvitationHandlerController } from './invitation-handler.controller';
 
 @Module({
-  controllers: [InvitationHandlerController],
-  providers: [InvitationHandlerService]
+  providers: [InvitationHandlerService],
 })
 export class InvitationHandlerModule {}
