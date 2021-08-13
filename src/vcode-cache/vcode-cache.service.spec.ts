912aa3dcedc7cdafe8547a0a7a18b8ced506e2a2
WhyYouAre <whyouare556@protonmail.com>
2021-08-13 16:49:27 +0800

    test: VcodeCacheService & VerificatinCodeService

--- src/vcode-cache/vcode-cache.service.spec.ts
+++ src/vcode-cache/vcode-cache.service.spec.ts
@@ -1,11 +1,13 @@
+import { CacheModule } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { VcodeCacheService } from './vcode-cache.service';
 
-describe('RedisCacheService', () => {
+describe('VcodeCacheService', () => {
   let service: VcodeCacheService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [CacheModule.register()],
       providers: [VcodeCacheService],
     }).compile();
 
@@ -14,5 +16,22 @@ describe('RedisCacheService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('setWithTTL', () => {
+    it('should be valid in ttl', async () => {
+      jest.useFakeTimers();
+      await service.setWithTTL('test_key', 123, 1);
+      jest.advanceTimersByTime(800);
+      const value = await service.get('test_key');
+      expect(value).toBe(123);
+    });
+    it('should be undefined after expiration', async () => {
+      jest.useFakeTimers();
+      await service.setWithTTL('test_key', 123, 1);
+      jest.advanceTimersByTime(1100);
+      const value = await service.get('test_key');
+      expect(value).toBeUndefined();
+    });
   });
 });
