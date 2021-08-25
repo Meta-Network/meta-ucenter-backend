dedd1f6f39f28cbbb15ce0e3c11933b3554dcdeb
Casimir Crystal <casimir.crystal.blue@outlook.com>
2021-08-25 18:57:52 +0800

    fix: doesn't need to set allowed origins

--- src/main.ts
+++ src/main.ts
@@ -40,14 +40,9 @@ async function bootstrap() {
 
   app.use((req, res, next) => {
     res.header(
-      'Access-Control-Allow-Origin',
-      configService.get<string[]>('cors.origins'),
-    );
-    res.header(
       'Access-Control-Allow-Methods',
       'POST, GET, OPTIONS, DELETE, PATCH, HEAD',
     );
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
     next();
   });
 
